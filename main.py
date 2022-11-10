0ddea82d241facbca35c991c6850cf60287f8a8d
ebanh <ebanhernandez@gmail.com>
2022-11-10 23:05:52 +0900

    organize code

--- main.py
+++ main.py
@@ -15,20 +15,21 @@ def num_characters(contents):
 
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
+    
     word_count = count_words(file_contents)
     char_count = num_characters(file_contents)
-    print("--- Being report of books/frankenstein.txt ---")
-    print(f"{word_count} words found in the document")
-    print()
-    
     char_list = list(char_count.items())
+    
     char_only = []
     for char in char_list:
         if char[0].isalpha():
             char_only.append(char)
-    
     char_only.sort()
-    
+
+# Print report
+    print("--- Being report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    print()
     for char in char_only:
         print(f"The '{char[0]}' character was found {char[1]} times")
     print("--- End report ---")
