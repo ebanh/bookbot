75f837f988251465dc5950270a3e774315d9c8b1
ebanh <ebanhernandez@gmail.com>
2022-11-10 22:50:23 +0900

    project 1 pull data from text, run report of word count and count of each alpha character in sorted order

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("hello world")
+def count_words(contents):
+    words = contents.split()
+    return len(words)
+
+def num_characters(contents):
+    text = {}
+    for letter in contents:
+        let = letter.lower()
+        if let not in text:
+            text[let] = 1
+        else:
+            text[let] += 1
+    return text
+
+
+with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+    word_count = count_words(file_contents)
+    char_count = num_characters(file_contents)
+    print("--- Being report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    print()
+    
+    char_list = list(char_count.items())
+    char_only = []
+    for char in char_list:
+        if char[0].isalpha():
+            char_only.append(char)
+    
+    char_only.sort()
+    
+    for char in char_only:
+        print(f"The '{char[0]}' character was found {char[1]} times")
+    print("--- End report ---")
